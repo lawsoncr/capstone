8bf492328fc2651d1cd73c7aa4fafb13586f39d8
lawsoncr <lawsoncr@appstate.edu>
2021-09-04 23:08:02 -0400

    got character to move in direction of awsd keys and added camera smoothing

--- RoR_like_game/Assets/ThirdPersonMovement.cs
+++ RoR_like_game/Assets/ThirdPersonMovement.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public CharacterContoller controller;
+    public float speed = 6f;
+    float turnSmoothVelocity;
+    
+    public float turnSmoothTime = 0.1f;
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f){
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            controller.Move(direction * speed * Time.deltaTime);
+        }
     }
 }
