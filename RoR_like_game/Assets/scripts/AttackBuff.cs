afbc9fc792b9584018c06eb4aba6382fd48c10ca
lawsoncr <lawsoncr@appstate.edu>
2021-10-28 14:34:40 -0400

    fixing merge.....again

--- RoR_like_game/Assets/scripts/AttackBuff.cs
+++ RoR_like_game/Assets/scripts/AttackBuff.cs
@@ -5,20 +5,6 @@ using UnityEngine;
 public class AttackBuff : MonoBehaviour
 {
     public GameObject item;
-<<<<<<< HEAD
-    public GameObject gun;
-    public float plusAttack = 15f;
-    void Awake()
-    {
-        gun = GameObject.FindGameObjectWithTag("Gun");
-    }
-
-    void Update()
-    {
-        //Compares the tag of the object entering this collider.
-        if(item == null){
-            gun.GetComponent<GunScript>().playerAttack(plusAttack);
-=======
     public float plusAttack = 15f;
     public GameObject gun;
     // Start is called before the first frame update
@@ -35,7 +21,6 @@ public class AttackBuff : MonoBehaviour
             GunScript gunDamage = gun.GetComponent<GunScript>();
             gunDamage.damageEnemy += 15f;
             Destroy(item);
->>>>>>> f9025789e4a9435a5115ff8fd867ec7c05830edb
         }
     }
 }
